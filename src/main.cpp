d7402b82c309d3244a426b1f9f32da3782fb99c2
Raviv Pelman <ryp323@gmail.com>
2025-07-27 09:19:02 -0400

    Add vertex and index buffer classes and update CMake configuration

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <Shader.h>
 #include <iostream>
-
+#include <vertexbuffer.h>
+#include <indexbuffer.h>
 
 int main(void)
 {
@@ -53,32 +54,20 @@ float texCoords[] = {
     // top-center corner
 };
    
+unsigned int vao;
+glGenVertexArrays(1,&vao);
+glBindVertexArray(vao);
 
 
-unsigned int VAO, VBO, EBO;
-glGenVertexArrays(1, &VAO);
-glGenBuffers(1, &VBO);
-glGenBuffers(1, &EBO);
 
-// Bind VAO
-glBindVertexArray(VAO);
+VertexBuffer vb(vertices,sizeof(vertices));
 
-// Bind and set VBO
-glBindBuffer(GL_ARRAY_BUFFER, VBO);
-glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-// Bind and set EBO
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-// Set vertex attribute pointers
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 glEnableVertexAttribArray(0);
+glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,0);
 
-// Unbind VBO (optional)
-glBindBuffer(GL_ARRAY_BUFFER, 0);
-// Unbind VAO (optional)
-glBindVertexArray(0);
+IndexBuffer ib(indices , 6);
+
+
 
 
     /* Loop until the user closes the window */
@@ -88,9 +77,10 @@ glBindVertexArray(0);
         glClear(GL_COLOR_BUFFER_BIT);
 
         
-    
+    ib.Bind();
+    vb.Bind();
     // Bind VAO and draw
-    glBindVertexArray(VAO);
+    glBindVertexArray(vao);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
