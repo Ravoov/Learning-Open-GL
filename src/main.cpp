9731dd8f705a6ff115c42baef40fb6831f78f4e7
Raviv Pelman <ryp323@gmail.com>
2025-07-29 18:37:47 -0400

    made the sqare change color

--- src/main.cpp
+++ src/main.cpp
@@ -18,25 +18,24 @@ unsigned int indices[] = {  // note that we start from 0!
     1, 2, 3    // second triangle
 };  
 
-
+float color = 0.1f;
 
 Shader ourShader("shaders/basic.vert", "shaders/basic.frag");
 ourShader.use();
-ourShader.setFloat("u_Color",1.0f,1.5f,0.0f,1.0f);
+
 VertexBuffer vb(vertices,sizeof(vertices));
 vertexArray vao(0,3,GL_FLOAT,GL_FALSE,sizeof(float)* 3 , 0, &vb);
 
 IndexBuffer ib(indices , 6);
-
-
-
-
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(renderer.m_window))
     {
         /* Render here */
     renderer.clear();
     renderer.draw(vao,ib,ourShader);
+    ourShader.setFloat("u_Color",1.0f,color,color,1.0f);
+    color += 0.0001f;
+    if(color > 1.0f) color = 0.0f;
     renderer.update();
     }
     glfwTerminate();
